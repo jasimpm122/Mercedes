4cb2073f449b02048f2d6d344319b08d53350551
hrithik-at-105410518218 <hrithikpramod9@gmail.com>
2024-10-28 22:43:36 +0530

    chnage

--- src/components/MockData.js
+++ src/components/MockData.js
@@ -69,7 +69,28 @@ export  const cars = [
     ,title: 'Mercedes-Benz C 63 AMG',
     price: 'Ab 36.980 €',
     description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
+  },
+
+
+ {
+    images: [
+      'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=2&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=3&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=4&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=4&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=5&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=6&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=7&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=8&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=9&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=10&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=11&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=12&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=7991158&bid=3760&format=xlrm&ino=12&app=Kiste-Default'
+
+    ]
+    ,title: 'Mercedes-Benz A 200 1.6T',
+    price: 'Ab 13.500 €',
+    description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
   }
-
-
 ]
