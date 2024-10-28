9ea86893f41dc1a7e40571685750b2edde6b22cc
hrithik-at-105410518218 <hrithikpramod9@gmail.com>
2024-10-28 18:56:51 +0530

    changes

--- src/components/MockData.js
+++ src/components/MockData.js
@@ -1,16 +1,21 @@
 export  const cars = [
   {
     images: [
-    require('../images/1.jpg'),
-    require('../images/2.jpg'),
-    require('../images/3.jpg'),
-    require('../images/4.jpg'),
-    require('../images/5.jpg'),
-    require('../images/6.jpg'),
-    require('../images/7.jpg'),
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=2&app=Kiste-Default',
+     'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=3&app=Kiste-Default',
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=4&app=Kiste-Default',
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=5&app=Kiste-Default',
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=6&app=Kiste-Default',
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=7&app=Kiste-Default',
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=8&app=Kiste-Default',
+    'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=9&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=10&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=11&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=12&app=Kiste-Default',
+'https://img.cargate360.de/default.aspx?vid=6177698&bid=3773&format=l&ino=13&app=Kiste-Default'
     ]
-    ,title: 'Die Mercedes-Benz A-Klasse Limousine',
-    price: 'Ab 64.300 €',
+    ,title: 'Mercedes-Benz CLA 35 AMG Shooting Brake 4Matic LED Navi Keyless Dyn. Kurvenlicht Parklenkass.',
+    price: 'Ab 35.300 €',
     description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
   }
 
