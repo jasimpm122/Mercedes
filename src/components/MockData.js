844457c1ddc38eee493ad7f19e208650698279f0
hrithik-at-105410518218 <hrithikpramod9@gmail.com>
2024-10-28 19:11:18 +0530

    change

--- src/components/MockData.js
+++ src/components/MockData.js
@@ -19,4 +19,31 @@ export  const cars = [
     description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
   }
 
+  ,
+
+  {
+    images: [
+      'https://vehicle.img.symfio.de/640x,q85/vehicle/qs0a8bB/4Pie2h/66d97c402cb560.82899169.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c3fb0a138.70651850.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c402cb560.82899169.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c40440176.50448257.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c40546cb7.23358545.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c407131c0.41577500.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c407131c0.41577500.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c40a656c0.65392116.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c40b9d6e3.02256657.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c40d338d3.99525840.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c40ed6159.15320476.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c41279285.64015705.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c413735e7.47171262.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c414730b6.38864787.jpg',
+'https://vehicle.img.symfio.de/1600x,q90/vehicle/qs0a8bB/4Pie2h/66d97c41682334.27561411.jpg'
+
+    ]
+    ,title: 'Mercedes-Benz B 220 d AMG-Line AUTOMATIK / NAVI / SITZHEIZUNG / 19-ZOLL / KAMERA',
+    price: 'Ab 30.300 €',
+    description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
+  }
+
+
 ]
