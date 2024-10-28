02d58cf269a28ec90943e13b2da60c71e51c21ca
hrithik-at-105410518218 <hrithikpramod9@gmail.com>
2024-10-28 19:19:10 +0530

    change

--- src/components/MockData.js
+++ src/components/MockData.js
@@ -43,6 +43,32 @@ export  const cars = [
     ,title: 'Mercedes-Benz B 220 d AMG-Line AUTOMATIK / NAVI / SITZHEIZUNG / 19-ZOLL / KAMERA',
     price: 'Ab 30.300 €',
     description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
+  },
+
+
+  {
+    images: [
+      'https://img.classistatic.de/api/v1/mo-prod/images/e6/e6ca968f-917a-45d5-a4bf-6c832e40059b?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/b4/b4f8dca9-04e7-460b-96e6-81ca337383e2?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/ef/efa0a31e-1195-442e-a15a-87657cf8eaf1?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/ef/efa0a31e-1195-442e-a15a-87657cf8eaf1?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/20/20d46158-812d-49c3-b0f4-3f6df635e750?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/36/368fcfc4-6021-4979-b4dc-558df22e66b1?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/5a/5a7f11f2-8569-4ad8-87ed-3a5c3e8aae19?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/5f/5fa34651-ccaf-4186-8c95-70063b156d1d?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/ae/ae76effb-899c-425d-bf7e-96c9ec7a04d9?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/ad/adc1e36a-cd9b-4a4d-9df3-40622993b47d?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/11/11756cd1-b2ab-4e6b-ba56-e1bda5aea6d6?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/23/2330405c-804a-4a86-9ccb-603bff625968?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/19/1909cd32-ecd0-45bd-953d-9dd2a5897c76?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/27/2733861b-5b87-44ef-a394-73553eb363a2?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/1a/1a23dcb4-978b-4e0c-8a39-fb4693cc6749?rule=mo-640.jpg',
+'https://img.classistatic.de/api/v1/mo-prod/images/ef/efb44d5e-7771-4209-b36d-b95196e6333f?rule=mo-640.jpg'
+
+    ]
+    ,title: 'Mercedes-Benz C 63 AMG',
+    price: 'Ab 36.980 €',
+    description: 'Die A-Klasse ist ein Auto, das begeistert.Das revolutionäre Infotainment-System MBUX, welches durch künstliche Intelligenz die Vorlieben des Fahrers lernt, maximale Connectivity bietet, navigiert, korrigiert und aushilft macht sie zu einem einmaligen Fahrzeug in der Kompaktklasse. Mit "Hey Mercedes" reichen nun zwei Worte, um das Fahrzeug nach Belieben zu steuern - Navigation, Klimaanlage, Sitzheizung, Musik, Design und vieles mehr lassen sich durch die Sprachbefehle intuitiv bedienen. Der Touchpad auf der Mittelkonsole, die zwei hochauflösenden Touchscreens auf dem Armaturenbrett und die Touchpads am Multifunktions-Lenkrad helfen ebenfalls bei der Bedienung der zahlreichen Funktionen. MBUX speichert die präferierten Einstellungen des Fahrers, passt sich an ihn an, lernt ihn kennen. Und auf Wunsch hilft es mit dem In-Car-Office sogar, den Arbeitstag zu organisieren. All das findet sich nun auch in einer noch erwachseneren Stufenheck-Variante der A-Klasse. '
   }
 
 
